7a94f575857517c87c314337fe649b2149170ef0
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2021-09-29 00:18:48 +0200

    for adding new item to list: converted JavaScript code into jQuery

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 
 function newItem(){
 
-//javascript
 //1. Adding a new item to the list of items:
-   let li = document.createElement("li");
+
+//JavaScript:
+
+/*   let li = document.createElement("li");
    let inputValue = document.getElementById("input").value;
    let text = document.createTextNode(inputValue);
    li.appendChild(text);
@@ -14,6 +16,20 @@ function newItem(){
      let list = document.querySelector('#list');
      list.appendChild(li);
    }
+*/
+
+//jQuery:
+
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(li);
+  }
+
 
  //2. Crossing out an item from the list of items:
    function crossOut() {
