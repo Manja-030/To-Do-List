d300f68a3be5023cb62439c3a962bb82097f6435
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2021-09-29 00:41:09 +0200

    fixed indentation with Atom Beautify

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
-
 function newItem() {
 
-//1. Adding a new item to the list of items:
+  //1. Adding a new item to the list of items:
 
-//JavaScript:
+  //JavaScript:
 
-/*   let li = document.createElement("li");
-   let inputValue = document.getElementById("input").value;
-   let text = document.createTextNode(inputValue);
-   li.appendChild(text);
+  /*   let li = document.createElement("li");
+     let inputValue = document.getElementById("input").value;
+     let text = document.createTextNode(inputValue);
+     li.appendChild(text);
 
-   if (inputValue === '') {
-     alert("You must write something!");
-   } else {
-     let list = document.querySelector('#list');
-     list.appendChild(li);
-   }
-*/
+     if (inputValue === '') {
+       alert("You must write something!");
+     } else {
+       let list = document.querySelector('#list');
+       list.appendChild(li);
+     }
+  */
 
-//jQuery:
+  //jQuery:
 
   let li = $("<li></li>");
   let inputValue = $("#input").val();
@@ -31,50 +30,50 @@ function newItem() {
   }
 
 
- //2. Crossing out an item from the list of items:
+  //2. Crossing out an item from the list of items:
 
- //JavaScript:
+  //JavaScript:
 
-/*   function crossOut() {
- 		li.classList.toggle("strike");
- 	}
+  /*   function crossOut() {
+   		li.classList.toggle("strike");
+   	}
 
- 	li.addEventListener("dblclick",crossOut);
-*/
+   	li.addEventListener("dblclick",crossOut);
+  */
 
-//jQuery:
+  //jQuery:
 
-li.on("dblclick", function() {
-  li.toggleClass("strike");
-});
+  li.on("dblclick", function() {
+    li.toggleClass("strike");
+  });
 
- //3. Adding the delete button "X":
+  //3. Adding the delete button "X":
 
- //JavaScript:
-/*
-   let crossOutButton = document.createElement("crossOutButton");
- 	crossOutButton.appendChild(document.createTextNode("X"));
- 	li.appendChild(crossOutButton);
+  //JavaScript:
+  /*
+     let crossOutButton = document.createElement("crossOutButton");
+   	crossOutButton.appendChild(document.createTextNode("X"));
+   	li.appendChild(crossOutButton);
 
- 	crossOutButton.addEventListener("click", deleteListItem);
- //Adding CLASS DELETE (DISPLAY: NONE) from the css:
-   function deleteListItem(){
- 		li.classList.add("delete")
- 	}
-*/
+   	crossOutButton.addEventListener("click", deleteListItem);
+   //Adding CLASS DELETE (DISPLAY: NONE) from the css:
+     function deleteListItem(){
+   		li.classList.add("delete")
+   	}
+  */
 
-//jQuery:
+  //jQuery:
 
-let crossOutButton = $("<crossOutButton></crossOutButton>");
-crossOutButton.append(document.createTextNode("X"));
-li.append(crossOutButton);
-crossOutButton.on("click", function() {
-  li.addClass("delete");
-});
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
+  crossOutButton.on("click", function() {
+    li.addClass("delete");
+  });
 
 
- // 4. Reordering the items:
-   $('#list').sortable();
+  // 4. Reordering the items:
+  $('#list').sortable();
 
 }
 
@@ -95,7 +94,7 @@ crossOutButton.on("click", function() {
 
 
 
-  /*
+/*
 // jQuery Code
 //1. Adding a new item to the list:
 
