f8920159426d2aec4f932d12f40682b11b15686a
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2021-09-29 00:24:44 +0200

    for crossing out an item: converted JS code into jQuery

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-function newItem(){
+function newItem() {
 
 //1. Adding a new item to the list of items:
 
@@ -32,11 +32,21 @@ function newItem(){
 
 
  //2. Crossing out an item from the list of items:
-   function crossOut() {
+
+ //JavaScript:
+
+/*   function crossOut() {
  		li.classList.toggle("strike");
  	}
 
  	li.addEventListener("dblclick",crossOut);
+*/
+
+//jQuery:
+
+li.on("dblclick", function() {
+  li.toggleClass("strike");
+});
 
  //3(i). Adding the delete button "X":
    let crossOutButton = document.createElement("crossOutButton");
